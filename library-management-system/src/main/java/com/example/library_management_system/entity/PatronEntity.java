ea8d3ec16eaee7615364f1b944b68c12c8497596
ZKartadimaja <zulfikarzamzam13@gmail.com>
2024-09-11 10:49:42 +0700

    Update PatronEntity.java

--- library-management-system/src/main/java/com/example/library_management_system/entity/PatronEntity.java
+++ library-management-system/src/main/java/com/example/library_management_system/entity/PatronEntity.java
@@ -1,4 +1,31 @@
 package com.example.library_management_system.entity;
 
+@Table(name = "patrons")
+@Setter
+@Getter
 public class PatronEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @JsonProperty("name")
+    private String name;
+
+    @Column(nullable = false)
+    @JsonProperty("email")
+    private String email;
+
+    @Column(nullable = false)
+    @JsonProperty("membership_type")
+    private String membershipType;
+
+    @Column(nullable = false)
+    @JsopProperty("crated_at")
+    private Date createdAt;
+
+    @Column(nullavle = false)
+    @JsonProperty('updated_at')
+    private Date updatedAt;
+
 }
