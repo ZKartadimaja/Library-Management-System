de159796a91975f5eaae8932fed8cadf57cadfc5
10lalala <bryanfelix2001@gmail.com>
2024-09-11 13:21:28 +0700

    Update Controller and Service Patron

--- library-management-system/src/main/java/com/example/library_management_system/service/PatronService.java
+++ library-management-system/src/main/java/com/example/library_management_system/service/PatronService.java
@@ -1,4 +1,15 @@
 package com.example.library_management_system.service;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public interface PatronService {
+
+    PatronResponse savePatron(PatronRequest patron);
+    PatronResponse updatePatron(Long patronId, PatronRequest patron);
+    PatronResponse getPatronHistoryById(Long patronId);
+    PatronResponse getPatronsById(Long patronId);
+    PatronResponse deletePatron(Long patronId);
+    PatronResponse getPatronCurrentBorrowingsById(Long patronId);
+
 }
