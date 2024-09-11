768bfe1fc5d3c9d00a2bb686b2b779020cb59301
tasyaraishaniaa <tasyaraishania@gmail.com>
2024-09-11 14:21:43 +0700

    Create Book Service Impl

--- library-management-system/src/main/java/com/example/library_management_system/service/BookService.java
+++ library-management-system/src/main/java/com/example/library_management_system/service/BookService.java
@@ -9,29 +9,29 @@ import org.springframework.stereotype.Component;
 @Component
 public interface BookService {
 
-    // Add a New Book
-    ResponseEntity<ApiResponse> saveBook(CreateBookRequest book);
-
-    //Update Book Details
-    ResponseEntity<ApiResponse> updateBook(UpdateBookRequest bookDetails);
-
-    //Get All Available Books
-    Page<ApiResponse> getAllAvailableBooks(Pageable pageable);
-
-    //Search Books by Title or Author
-    Page<ApiResponse> getBooksByKeyword(String keyword, Pageable pageable);
-
-    //Get Overdue Books
-    Page<ApiResponse> getOverdueBooks(Pageable pageable);
-
-    //Check Available Copies of Book
-    ResponseEntity<ApiResponse> getAvailableBookCopiesById(Long bookId);
-
-    //Delete Book By Id
-    ResponseEntity<ApiResponse> deleteBook (Long bookId);
-
-    //Get a Book By Id
-    BookEntity getBookById(Long bookId);
+//    // Add a New Book
+//    ResponseEntity<ApiResponse> saveBook(CreateBookRequest book);
+//
+//    //Update Book Details
+//    ResponseEntity<ApiResponse> updateBook(UpdateBookRequest bookDetails);
+//
+//    //Get All Available Books
+//    Page<ApiResponse> getAllAvailableBooks(Pageable pageable);
+//
+//    //Search Books by Title or Author
+//    Page<ApiResponse> getBooksByKeyword(String keyword, Pageable pageable);
+//
+//    //Get Overdue Books
+//    Page<ApiResponse> getOverdueBooks(Pageable pageable);
+//
+//    //Check Available Copies of Book
+//    ResponseEntity<ApiResponse> getAvailableBookCopiesById(Long bookId);
+//
+//    //Delete Book By Id
+//    ResponseEntity<ApiResponse> deleteBook (Long bookId);
+//
+//    //Get a Book By Id
+//    BookEntity getBookById(Long bookId);
 
 
 }
--- library-management-system/src/main/java/com/example/library_management_system/service/impl/BookServiceImpl.java
+++ library-management-system/src/main/java/com/example/library_management_system/service/impl/BookServiceImpl.java
@@ -1,4 +1,31 @@
 package com.example.library_management_system.service.impl;
 
-public class BookServiceImpl {
+import com.example.library_management_system.entity.BookEntity;
+import com.example.library_management_system.repository.BookRepository;
+import com.example.library_management_system.service.BookService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class BookServiceImpl implements BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    //Add a New Book
+//    @Override
+//    public ResponseEntity<ApiResponse<Object>> saveBook(CreateBookRequest book) {
+//
+//        BookEntity newBook = new BookEntity();
+//        newBook.setTitle(book.getTitle());
+//        newBook.setAuthor(book.getAuthor());
+//        newBook.setIsbn(book.getIsbn());
+//        newBook.setQuantity(book.getQuantity());
+//
+//        bookRepository.save(newBook);
+//    }
+//
 }
