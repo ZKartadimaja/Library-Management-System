582b30ad58fc1e85d49603bbd2da84eeec0f4586
ZKartadimaja <zulfikarzamzam13@gmail.com>
2024-09-11 11:39:07 +0700

    fix email unique

--- library-management-system/src/main/java/com/example/library_management_system/entity/PatronEntity.java
+++ library-management-system/src/main/java/com/example/library_management_system/entity/PatronEntity.java
@@ -20,7 +20,7 @@ public class PatronEntity {
     @JsonProperty("name")
     private String name;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     @JsonProperty("email")
     private String email;
 
